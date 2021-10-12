ee3e58877ba027779739283065b48e345bff7fa4
yangrunqiu <yangrunqiu.richer@bytedance.com>
2021-10-12 16:57:54 +0800

    打开dataBinding

--- app/src/main/java/com/richer/richers/richer_wa/MainActivity.java
+++ app/src/main/java/com/richer/richers/richer_wa/MainActivity.java
@@ -1,14 +1,19 @@
 package com.richer.richers.richer_wa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.richer.richers.richer_wa.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
