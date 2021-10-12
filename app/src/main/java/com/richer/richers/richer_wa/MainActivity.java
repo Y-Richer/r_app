89b18b100e0556cedff8da7d9d9bdc4184989bb6
yangrunqiu <yangrunqiu.richer@bytedance.com>
2021-10-12 16:50:36 +0800

    Initial commit

---
+++ app/src/main/java/com/richer/richers/richer_wa/MainActivity.java
@@ -0,0 +1,14 @@
+package com.richer.richers.richer_wa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
